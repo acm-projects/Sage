127ff599a4274b3dfc893930b41640aa55134ffb
Maruthi Eranki <maruthieranki07@gmail.com>
2024-02-28 16:43:14 -0600

    test

---
+++ advisabot/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Header />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ advisabot/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+
+  return (
+    <header>
+      <h1>Advisabot</h1>
+    </header>
+  )
+}
+
+export default Header
