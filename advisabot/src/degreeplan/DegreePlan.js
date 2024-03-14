3025a977eea333d7f080d98b3760181a6ae5b0ab
Shraddha Subash <shraddhasubashpublic@gmail.com>
2024-03-14 00:39:48 -0500

    added a lil to deg plan pg

--- advisabot/src/degreeplan/DegreePlan.js
+++ advisabot/src/degreeplan/DegreePlan.js
@@ -1,13 +1,20 @@
-import React from 'react'
-
+// DegreePlan.js
+import React from 'react';
+import './style.css';
 
 const DegreePlan = () => {
   return (
-    <div>
-      
-      degree plan page
+    <div className='mainDiv'>
+      <div className='content'>
+        <div className='mainBox'>
+          <div className='allClasses'>
+            <p className=' specialHeader generalFont'>all classes</p>
+            {/* Content inside allClasses */}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DegreePlan
+export default DegreePlan;
